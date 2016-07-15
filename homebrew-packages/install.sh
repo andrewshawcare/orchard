394f8fab0ee8bc624db0ef1e89dcb11b68fc57e7
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-07-15 13:51:02 -0400

    Fix shellcheck errors and warnings

--- homebrew-packages/install.sh
+++ homebrew-packages/install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+package_list=()
+# shellcheck source=./
 source "$(dirname "$0")/package-list.bash"
 
 brew tap caskroom/cask
 brew tap caskroom/fonts
 brew update
 
-for package in ${package_list[@]}; do
+for package in "${package_list[@]}"; do
   if ! brew cask list ${package}; then
     brew cask install ${package}
   fi
--- homebrew-packages/test.bats
+++ homebrew-packages/test.bats
@@ -3,7 +3,7 @@
 load package-list
 
 @test "homebrew packages are installed" {
-  for package in ${package_list[@]}; do
+  for package in "${package_list[@]}"; do
     run brew cask list "${package}"
     [ "${status}" -eq 0 ]
   done
