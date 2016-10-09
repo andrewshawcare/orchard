250decb3e7f80143d64e36b2e487a9b42b453b38
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-10-09 12:08:26 -0400

    Simplify provisioning script

--- provision.sh
+++ provision.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 
-cd ./bats && ./install.sh && cd ..
-cd ./homebrew && ./install.sh && cd ..
-cd ./brew-packages && ./install.sh && cd ..
-cd ./homebrew-packages && ./install.sh && cd ..
-cd ./atom && ./install.sh && cd ..
-cd ./mac-app-store && ./install.sh && cd ..
+find . -name 'install.sh' -exec '{}' \;
