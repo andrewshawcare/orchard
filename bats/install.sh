66f4a6340722538b1e9118c1fc62166998f78e7b
Andrew Shaw Care <andrew@MacBook-Pro.lan>
2016-10-10 14:28:02 -0400

    Fixed bats installation

--- bats/install.sh
+++ bats/install.sh
@@ -2,9 +2,8 @@
 set -e
 
 if ! type bats; then
-  git clone https://github.com/sstephenson/bats.git
-  cd bats
-  sudo ./install.sh /usr/local
-  cd ..
-  rm -rf bats
+  mkdir -p /tmp/bats
+  git clone https://github.com/sstephenson/bats.git /tmp/bats
+  sudo /tmp/bats/install.sh /usr/local
+  rm -rf /tmp/bats
 fi
