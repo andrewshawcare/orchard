e9dacf3a282d7875c9a21ea4513ff41f1c50dc03
Andrew Shaw Care <andrewshawcare@users.noreply.github.com>
2017-08-11 18:26:10 -0400

    Update package-list.bash

--- cask-packages/package-list.bash
+++ cask-packages/package-list.bash
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 package_list=(\
   1password \
-  atom \
   docker \
   firefox \
   google-chrome \
   google-drive \
+  hipchat \
   intellij-idea \
   java \
-  nordvpn
+  nordvpn \
+  tunnelblick \
+  zoomus
 )
 export package_list
