a7ae4e8af954c1db2818d0acc27b7b1219d7d081
Andrew Shaw Care <andrewshawcare@users.noreply.github.com>
2018-01-12 11:59:52 -0500

    Update package-list.bash

--- cask-packages/package-list.bash
+++ cask-packages/package-list.bash
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 package_list=(\
-  1password \
   docker \
-  firefox \
   google-chrome \
   google-backup-and-sync \
   karabiner-elements \
-  mattermost \
   sublime-text \
   tunnelblick \
   vagrant \
