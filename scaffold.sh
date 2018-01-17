eb0122bbf1a3222468dc39b94849c2c35abd2361
Andrew Shaw Care <andrew@Andrews-MacBook-Pro.local>
2018-01-17 15:00:50 -0500

    Add scaffolding

---
+++ scaffold.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+print_usage() {
+  echo "Usage: ${0} <resource-name>"
+}
+resource_name="${1?"$(print_usage)"}"
+
+mkdir "$resource_name"
+
+cat <<HEREDOC > "$resource_name/test.bats"
+#!/usr/bin/env bats
+@test 'Implement me!' {
+  [ 0 -eq 1 ]
+}
+HEREDOC
+
+echo '#!/bin/sh -e' > "$resource_name/install.sh"
+
+chmod +x "$resource_name/install.sh"
