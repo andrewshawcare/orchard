d9648eef9584b31753a3e391f7d3409da4d88347
Andrew Shaw Care <andrew@Andrews-MacBook-Pro.local>
2018-01-21 20:42:47 -0500

    Change SF Pro format

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-defaults write com.apple.Terminal 'Window Settings' -dict-add 'SF Pro' "$(cat sf-pro.plist)"
+defaults write com.apple.Terminal 'Window Settings' -dict-add 'SF Pro' -dict "$(cat sf-pro.plist)"
 defaults write com.apple.Terminal 'Default Window Settings' 'SF Pro'
 defaults write com.apple.Terminal 'Startup Window Settings' 'SF Pro'
