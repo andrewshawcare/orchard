cf18abad7431ed146db42b040db543ade717ffc2
Andrew Shaw Care <andrewshawcare@users.noreply.github.com>
2018-02-18 16:27:25 -0500

    Delete install.sh

--- sublime-text-3/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-cloud_user_packages_path="${HOME}/Documents/Sublime Text 3/Packages/User"
-local_user_packages_path="${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
-rm -r "${local_user_packages_path}"
-ln -s "${cloud_user_packages_path}" "${local_user_packages_path}"
