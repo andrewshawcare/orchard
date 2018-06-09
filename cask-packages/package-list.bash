683dbbdf6c16f338ebc66f25cfd4b13532582c82
Andrew Shaw Care <andrewshawcare@users.noreply.github.com>
2018-06-09 13:59:33 -0400

    Update package-list.bash

--- cask-packages/package-list.bash
+++ cask-packages/package-list.bash
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 package_list=(\
-  box-sync \
   docker \
-  duet \
   google-chrome \
   google-backup-and-sync \
   intellij-idea \
   karabiner-elements \
-  mattermost \
   obs \
-  spotify \
   sublime-text \
   tunnelblick \
   vagrant \
   virtualbox \
-  vnc-viewer \
   zoomus
 )
 export package_list
