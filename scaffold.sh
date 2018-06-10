020f536814bebea30a108ba9ac00da62fd18432b
Andrew Shaw Care <andrew@Andrews-MacBook-Pro.local>
2018-06-09 21:41:29 -0400

    Fix scaffold formatting

--- scaffold.sh
+++ scaffold.sh
@@ -1,18 +1,21 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
 print_usage() {
   echo "Usage: ${0} <resource-name>"
 }
+
 resource_name="${1?"$(print_usage)"}"
 
 mkdir "$resource_name"
 
 cat <<HEREDOC > "$resource_name/test.bats"
 #!/usr/bin/env bats
+
 @test 'Implement me!' {
   [ 0 -eq 1 ]
 }
 HEREDOC
 
-echo '#!/bin/sh -e' > "$resource_name/install.sh"
+echo '#!/bin/bash -e' > "$resource_name/install.sh"
 
 chmod +x "$resource_name/install.sh"
