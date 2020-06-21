95b24b9d73a90ab2fe47da5e17bd6a3ba5eed08e
Andrew Shaw Care <andrew@MacBook-Pro.local>
2020-06-21 07:10:08 -0400

    Update homebrew install; Add OmniGraffle

--- brew/install.sh
+++ brew/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 if ! type brew; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   brew analytics off
 fi
--- mac-app-store/application-list.bash
+++ mac-app-store/application-list.bash
@@ -3,7 +3,8 @@
 application_list=(\
   '1Password 7 - Password Manager' \
   'BreakTime' \
-  'Divvy - Window Manager'
+  'Divvy - Window Manager' \
+  'OmniGraffle 7' \
 )
 
 export application_list
