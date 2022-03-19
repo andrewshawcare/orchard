e68d40a84a054688ffd8b17dd840d82a041881b0
Andrew Shaw Care <andrewshawcare@gmail.com>
2022-03-19 11:19:53 -0400

    Add dockutil; remove applications from dock

--- dock/install.sh
+++ dock/install.sh
@@ -4,3 +4,30 @@ working_directory=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 defaults write com.apple.dock autohide -boolean true
 killall Dock
+
+if ! which dockutil; then
+    brew install dockutil
+fi
+
+default_dock_applications_to_remove=(\
+    'Messages' \
+    'Maps' \
+    'Photos' \
+    'FaceTime' \
+    'Contacts' \
+    'Reminders' \
+    'Notes' \
+    'TV' \
+    'Music' \
+    'Podcasts' \
+    'News' \
+    'App Store' \
+    'System Preferences'
+)
+for application in "${default_dock_applications_to_remove[@]}"; do
+    if dockutil --list "${application}"; then
+        echo "Removing ${application} from the dock..."
+        dockutil --remove --no-restart "${application}"
+        echo "${application} removed from the dock."
+    fi
+done
