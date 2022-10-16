ae093cdf23e3fc9ab93e83f0f07173754805a1bb
Andrew Shaw Care <andrewshawcare@gmail.com>
2022-10-16 09:39:57 -0400

    Ensure profile has brew included in shell

--- brew/install.sh
+++ brew/install.sh
@@ -2,6 +2,8 @@
 
 if ! type brew; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "${HOME}/.zprofile"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew analytics off
