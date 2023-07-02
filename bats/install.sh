3df4f5f0714417b064e3ce4154e1ae0f4b032cc4
Andrew Shaw Care <andrewshawcare@gmail.com>
2023-07-02 08:18:42 -0400

    Switch to bats-core

--- bats/install.sh
+++ bats/install.sh
@@ -2,7 +2,7 @@
 
 if ! type bats; then
   tmp_dir=$(mktemp -d -t bats)
-  git clone https://github.com/sstephenson/bats.git "${tmp_dir}"
+  git clone https://github.com/bats-core/bats-core.git "${tmp_dir}"
   sudo "${tmp_dir}/install.sh" /usr/local
   rm -rf "${tmp_dir}"
 fi
