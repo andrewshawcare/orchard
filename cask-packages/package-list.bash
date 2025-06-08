30fab92a2b1669ef92fb75bc2e9af635ecf50466
Andrew Shaw Care <andrewshawcare@gmail.com>
2025-06-08 05:00:28 -0700

    Add additional font-iosevka variants to package list

--- cask-packages/package-list.bash
+++ cask-packages/package-list.bash
@@ -5,9 +5,13 @@ package_list=(\
   '1password-cli' \
   'docker' \
   'font-iosevka' \
+  'font-iosevka-aile' \
+  'font-iosevka-etoile' \
+  'font-iosevka-slab' \
   'google-chrome' \
   'jetbrains-toolbox' \
-  'rectangle'
+  'rectangle' \
+  'visual-studio-code'
 )
 
 export package_list
