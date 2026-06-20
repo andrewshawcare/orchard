7fbd3fb3ebd4c27c38f6f661c327b7aa0ccdcec5
Andrew Shaw Care <andrewshawcare@gmail.com>
2026-06-20 11:09:58 -0400

    Add additional fonts to the font list

--- fonts/font-list.bash
+++ fonts/font-list.bash
@@ -2,6 +2,19 @@
 
 font_list=(\
   'font-jetbrains-mono' \
+  'font-fira-code' \
+  'font-source-code-pro' \
+  'font-roboto-mono' \
+  'font-inconsolata' \
+  'font-ubuntu-mono' \
+  'font-cascadia-code' \
+  'font-iosevka' \
+  'font-terminus' \
+  'font-mononoki' \
+  'font-hack' \
+  'font-pt-mono' \
+  'font-oxygen-mono' \
+  'font-anonymous-pro' \
 )
 
 export font_list
